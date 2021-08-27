6da68d3edbeb965933a842c8e3726174f8c8da45
Djumabaevs <djumabaevb@gmail.com>
2021-08-27 13:22:05 +0600

    sealed class DataState

--- core/src/main/java/com/djumabaevs/core/DataState.kt
+++ core/src/main/java/com/djumabaevs/core/DataState.kt
@@ -2,4 +2,15 @@ package com.djumabaevs.core
 
 sealed class DataState<T> {
 
+    data class Response<T>(
+        val uiComponent: UIComponent
+    ): DataState<T>()
+
+    data class Data<T>(
+        val data: T? = null
+    ): DataState<T>()
+
+    data class Loading<T>(
+        val progressBarState: ProgressBarState = ProgressBarState.idle
+    ): DataState<T>()
 }
