c370ecafb53f5833a5e7fbf019d69009bf9db73f
Djumabaevs <djumabaevb@gmail.com>
2021-08-30 02:55:09 +0600

    val disposable

--- app/src/androidTest/java/com/djumabaevs/multimodulesdota/coil/FakeImageLoader.kt
+++ app/src/androidTest/java/com/djumabaevs/multimodulesdota/coil/FakeImageLoader.kt
@@ -1,2 +1,18 @@
 package com.djumabaevs.multimodulesdota.coil
 
+import android.content.Context
+import coil.ImageLoader
+import coil.request.Disposable
+
+class FakeImageLoader {
+    companion object Factory {
+        fun build(context: Context): ImageLoader {
+
+            private val disposable = object: Disposable {
+                override val isDisposed get() = true
+                override fun dispose() {}
+                override suspend fun await() {}
+            }
+        }
+    }
+}
